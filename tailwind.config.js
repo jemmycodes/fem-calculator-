347d4e550f79774eabf61fa7aa1225b117887646
jemmy <flashyjotebele@gmail.com>
2022-11-02 09:49:55 -1100

    testing design done --theme 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         toggleBackgroundKeypadBackground: "hsl(223, 31%, 20%)",
         screenBackground: "hsl(224, 36%, 15%)",
         keyBackground: "hsl(225, 21%, 49%)",
-        keyShadow: "hsl(224, 28%, 35%)",
+        blueKeyShadow: "hsl(224, 28%, 35%)",
         keyBackgroundToggle: "hsl(6, 63%, 50%)",
         keyShadow: "hsl(6, 70%, 34%)",
         lightGrayishOrange: "hsl(30, 25%, 89%)",
